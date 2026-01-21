e92b13600d622a715e1a0a4a40af2b44446a1b37
altyapistaj <altyapi_staj@proton.me>
2026-01-21 14:25:08 +0300

    jobVariables.groovy added

---
+++ vars/jobVariables.groovy
@@ -0,0 +1,13 @@
+def call(String JOB_NAME){
+    return [
+            projectName : 'react-app',
+            jobName : JOB_NAME.tokenize('/')[1],
+            branch : "main",
+
+
+            customWorkspace : "workspace/" + JOB_NAME + "/",
+            gitAdressAndName : JOB_NAME.tokenize('/')[1]
+    ]
+
+}
+
